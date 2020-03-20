59e3b076a42622a9285230812a914734ea76a367
atmasark <moroatte@gmail.com>
2020-03-20 21:38:09 +0200

    add error handling for movieRouter endpoints

--- api/src/routes/movie.ts
+++ api/src/routes/movie.ts
@@ -7,23 +7,54 @@ const movieRouter = Router();
 movieRouter.get('/getAll', (req, res) => {
   // Keys needed for the list view
   const listKeys = ['id', 'title', 'poster'];
-
-  const listData = movieData.map((movie: Movie) => Object.keys(movie)
-    .filter((key) => listKeys.includes(key))
-    .reduce((obj, key) => {
-      obj[key] = movie[key];
-      return obj;
-    }, {}));
-
-  res.json(listData);
+  try {
+    const listData = movieData.map((movie: Movie) => Object.keys(movie)
+      .filter((key) => listKeys.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = movie[key];
+        return obj;
+      }, {}));
+    return res.status(200).json({
+      success: true,
+      msg: 'Movie list fetch successful',
+      data: listData,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      msg: 'Error occurred while fetching movie list',
+    });
+  }
 });
 
 
 movieRouter.get('/getSingle', (req, res) => {
   const { id } = req.query;
-  const queriedMovie = movieData.find((asd: Movie) => asd.id === parseInt(id));
-  if (queriedMovie) res.json(queriedMovie);
-  else res.json({ success: 'false', message: 'id not found' });
+  try {
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        msg: 'Id is required',
+      });
+    }
+    const queriedMovie = movieData.find((asd: Movie) => asd.id === parseInt(id));
+    if (!queriedMovie) {
+      return res.status(404).json({
+        success: false,
+        msg: 'Incorrect id',
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      msg: 'Single movie fetch successful',
+      data: queriedMovie,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      msg: 'Error occurred while fetching a movie',
+    });
+  }
 });
 
 export default movieRouter;
