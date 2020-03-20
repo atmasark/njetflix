6d99c05e3df1df235575b063f40a83d2a3ecb515
atmasark <moroatte@gmail.com>
2020-03-20 12:25:07 +0200

    add catcher for invalid routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,4 +6,7 @@ import express from 'express';
 const PORT = 2500;
 const app = express();
 
+// catch invalid routes
+app.all('*', (request, response) => response.sendStatus(404));
+
 app.listen({ port: PORT }, () => console.log(`Server up and running, listening to port ${PORT}`));
