3107cbb2f92621a52e3ee780ab0632eefe5e59a7
atmasark <moroatte@gmail.com>
2020-03-20 12:24:39 +0200

    initialize express server

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,9 @@
-console.log('Hello there!');
+/*
+ * Main entry point for the application
+ */
+import express from 'express';
+
+const PORT = 2500;
+const app = express();
+
+app.listen({ port: PORT }, () => console.log(`Server up and running, listening to port ${PORT}`));
