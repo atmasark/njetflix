46b4777fa97670be88bffdb7dde521b459d0bff6
atmasark <moroatte@gmail.com>
2020-03-20 16:21:14 +0200

    implement getSingle endpoint

--- api/src/routes/movie.ts
+++ api/src/routes/movie.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import movieData from '../utils/convertData';
+import Movie from '../interfaces/movie';
 
 const movieRouter = Router();
 
@@ -7,7 +8,7 @@ movieRouter.get('/getAll', (req, res) => {
   // Keys needed for the list view
   const listKeys = ['id', 'title', 'poster'];
 
-  const listData = movieData.map((movie) => Object.keys(movie)
+  const listData = movieData.map((movie: Movie) => Object.keys(movie)
     .filter((key) => listKeys.includes(key))
     .reduce((obj, key) => {
       obj[key] = movie[key];
@@ -15,6 +16,14 @@ movieRouter.get('/getAll', (req, res) => {
     }, {}));
 
   res.json(listData);
+});
+
+
+movieRouter.get('/getSingle', (req, res) => {
+  const { id } = req.query;
+  const queriedMovie = movieData.find((asd: Movie) => asd.id === parseInt(id));
+  if (queriedMovie) res.json(queriedMovie);
+  else res.json({ success: 'false', message: 'id not found' });
 });
 
 export default movieRouter;
