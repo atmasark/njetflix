3432116f0ad01349bc90173deb63af412a432a96
atmasark <moroatte@gmail.com>
2020-03-21 11:24:17 +0200

    add reducers for movie list fetching

---
+++ client/src/state/modules/movies/reducers.ts
@@ -0,0 +1,48 @@
+import {
+  FETCH_MOVIES_REQUEST,
+  FETCH_MOVIES_SUCCESS,
+  FETCH_MOVIES_FAILURE,
+  MovieState,
+  MovieActionTypes,
+} from './types';
+
+const initialState = {
+  list: {
+    isLoading: false,
+    data: [],
+  },
+};
+
+
+const fetchMoviesReducer = (state: MovieState = initialState, action: MovieActionTypes) => {
+  switch (action.type) {
+    case FETCH_MOVIES_REQUEST:
+      return {
+        ...state,
+        list: {
+          isLoading: true,
+          ...state.list,
+        },
+      };
+    case FETCH_MOVIES_SUCCESS:
+      return {
+        ...state,
+        list: {
+          isLoading: false,
+          data: action.list,
+        },
+      };
+    case FETCH_MOVIES_FAILURE:
+      return {
+        ...state,
+        list: {
+          isLoading: false,
+          ...state.list,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default fetchMoviesReducer;
