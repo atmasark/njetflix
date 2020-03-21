15af0ad911c545f02c62f28dfbb0038e2feb9017
atmasark <moroatte@gmail.com>
2020-03-21 11:23:39 +0200

    add types, start redux folder structure

---
+++ client/src/state/modules/movies/types.ts
@@ -0,0 +1,36 @@
+export const FETCH_MOVIES_REQUEST = 'FETCH_MOVIES_REQUEST';
+export const FETCH_MOVIES_SUCCESS = 'FETCH_MOVIES_SUCCESS';
+export const FETCH_MOVIES_FAILURE = 'FETCH_MOVIES_FAILURE';
+
+interface FetchMoviesRequestAction {
+  type: typeof FETCH_MOVIES_REQUEST
+}
+
+interface FetchMoviesSuccessAction {
+  type: typeof FETCH_MOVIES_SUCCESS
+  list: ListElement[]
+}
+
+interface FetchMoviesFailureAction {
+  type: typeof FETCH_MOVIES_FAILURE
+}
+
+export type MovieActionTypes =
+  | FetchMoviesRequestAction
+  | FetchMoviesSuccessAction
+  | FetchMoviesFailureAction
+
+export interface MovieState {
+  list: ListState,
+}
+
+interface ListState {
+  isLoading: boolean
+  data: ListElement[]
+}
+
+export interface ListElement {
+  id: number,
+  title: string,
+  poster: string
+}
