9747541da442609f01eef1963eab3f18f31fe249
atmasark <moroatte@gmail.com>
2020-03-21 11:26:05 +0200

    fetch movies to state after the initial render

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchMovies } from './state/modules/movies';
 import './index.css';
 
-export default () => (
-  <p> Hello world</p>
-);
+const App = ({ dispatch }: any) => {
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, []);
+  return (
+    <p> Hello world</p>
+  );
+};
+
+export default connect()(App);
