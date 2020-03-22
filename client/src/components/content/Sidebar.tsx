c767bb683401c2f7ff57be683ec03cdb99746a82
atmasark <moroatte@gmail.com>
2020-03-22 17:51:37 +0200

    move sidebar list to its parent, add active style

--- client/src/components/content/Sidebar.tsx
+++ client/src/components/content/Sidebar.tsx
@@ -1,29 +1,62 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import styled from 'styled-components';
-import Navigation from './sidebar/Navigation'
+import { Genre, ListElement } from '../types';
+import { setActiveGenre } from '../../state/modules/movies/thunks';
 import { getAllGenres } from '../../state/modules/movies/selectors';
-import { Genre, ListElement, State } from '../types';
 
 const Wrapper = styled.div`
   display: flex;
   flex: 1 1;
 `;
 
-const Sidebar = (props: { movies: ListElement[]; genres: Genre[] }) => {
-  const { movies, genres } = props;
+const List = styled.ul`
+  position: fixed;
+  top: 50%;
+  transform: translate(0,-50%);
+  list-style: none;
+`;
+
+const ListItem = styled.li.attrs((props: { isActive: boolean }) => ({
+  isActive: props.isActive,
+}))`
+  background: ${(props) => props.isActive && 'linear-gradient(90deg, rgba(255,0,0,0.5) 0%, rgba(255,255,255,0) 100%)'};
+  padding-left: ${(props) => props.isActive && '20px'};
+`;
+
+const Navigation = (props: { movies: ListElement[]; genres: Genre[]; activeGenre: string; setActiveGenre: any; }) => {
+  const { movies, genres, activeGenre, setActiveGenre } = props;
+  const handleOnClick = (genre: string) => {
+    setActiveGenre(genre)
+  }
   if (movies) {
     return (
       <Wrapper>
-        <Navigation genres={genres} />
+        <List>
+          {genres.map((genre: Genre) =>
+            <ListItem
+              isActive={genre.name === activeGenre}
+              onClick={() => handleOnClick(genre.name)}
+              key={genre.name}>
+              {genre.name}
+            </ListItem>)}
+        </List>
       </Wrapper>
     )
   } else return <></>
 };
 
-const mapStateToProps = (state: State) => ({
+
+const mapStateToProps = (state: any) => ({
   movies: state.movies.list.data,
   genres: getAllGenres(state),
-});
+  activeGenre: state.movies.activeGenre,
+})
 
-export default connect(mapStateToProps)(Sidebar);
+const mapDispatchToProps = (dispatch: any) => ({
+  setActiveGenre(genre: string) {
+    dispatch(setActiveGenre(genre))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
