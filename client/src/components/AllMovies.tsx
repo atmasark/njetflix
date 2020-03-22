863dec71f356446bffab673a42f76bd55f6e1903
atmasark <moroatte@gmail.com>
2020-03-23 00:00:31 +0200

    implement simple all movies page

--- client/src/components/AllMovies.tsx
+++ client/src/components/AllMovies.tsx
@@ -1,29 +1,54 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { ListElement, State } from './types';
+import { fetchCurrentMovie } from '../state/modules/movies/thunks';
 
 const Wrapper = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 1em;
+  padding: 100px 50px;
 `;
 
-const PlaceholderText = styled.p`
-font-size: 28px;
+const Movie = styled.div.attrs((props: { poster: string }) => ({
+  poster: props.poster,
+}))`
+  min-height: 450px;
+  min-width: 300px;
+  margin: 0px 10px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url(${(props) => props.poster});
 `;
 
-const AllMovies = (props: {
-  movies: ListElement[];
-}) => {
+const AllMovies = ({ history, ...props }:
+  {
+    history: any;
+    movies: ListElement[];
+    fetchCurrentMovie: (id: number) => void;
+  }) => {
   const {
     movies,
+    fetchCurrentMovie,
   } = props;
+
+  const handleClick = (id: number) => {
+    fetchCurrentMovie(id);
+    // history.push('/');
+  };
+
   return (
     <Wrapper>
-      {movies && (
-        <>
-          <PlaceholderText>All movies here</PlaceholderText>
-        </>
-      )}
+      {movies.map((movie: ListElement) => (
+        <Movie
+          onClick={() => handleClick(movie.id)}
+          key={movie.id}
+          poster={movie.poster}
+        />
+      ))}
     </Wrapper>
   );
 };
@@ -33,7 +58,9 @@ const mapStateToProps = (state: State) => ({
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-
+  fetchCurrentMovie(id: number) {
+    dispatch(fetchCurrentMovie(id));
+  },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(AllMovies);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AllMovies));
