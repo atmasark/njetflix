8dad0d067654aa5f7491e3090e4937b1325a1d9d
atmasark <moroatte@gmail.com>
2020-03-23 06:41:31 +0200

    add selectors for familyFilter & sorting

--- client/src/state/modules/movies/selectors.ts
+++ client/src/state/modules/movies/selectors.ts
@@ -3,6 +3,45 @@ import { Genre, ListElement, State } from './types';
 
 const moviesSelector = (state: State) => state.movies.list.data;
 
+export const movieSortingSelector = (state: State) => state.movies.list.sortBy;
+export const familyFilterSelector = (state: State) => state.movies.list.familyFilter;
+
+
+export const getSortedMovies = createSelector(
+  [movieSortingSelector, moviesSelector],
+  (moviesSortBy, movies) => {
+    switch (moviesSortBy) {
+      case 'IMDB_RATING':
+        return [...movies].sort(
+          (a, b) => Number(b.imdbRating)
+            - Number(a.imdbRating),
+        );
+      case 'YEAR':
+        return [...movies].sort(
+          (a, b) => Number(b.year)
+            - Number(a.year),
+        );
+      case 'AZ':
+        return [...movies].sort(
+          (a, b) => a.title.localeCompare(b.title),
+        );
+      default:
+        return [...movies].sort(
+          (a, b) => a.title.localeCompare(b.title),
+        );
+    }
+  },
+);
+
+export const getFamilyFilteredMovies = createSelector(
+  [familyFilterSelector, getSortedMovies],
+  (familyFilter, movies) => {
+    if (!familyFilter) return movies;
+    return movies.filter((movie) => ['G', 'PG', 'PG-13'].includes(movie.rated));
+  },
+);
+
+
 export const getAllGenres = createSelector(
   [moviesSelector],
   (movies) => {
